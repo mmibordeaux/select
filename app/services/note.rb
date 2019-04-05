3e96d54e1c9e535864ee87ee74ebf39cb3f41f79
Arnaud Levy <alevy@lespoupees.paris>
2019-04-05 17:30:17 +0200

    add note bac

--- app/services/note.rb
+++ app/services/note.rb
@@ -1,7 +1,7 @@
 class Note
   def self.average(row)
-    # Avoid row[95..96]
-    notes = row[20..94] + row[97..1000]
+    # Avoid row[96..97]
+    notes = [row[18]] + row[21..95] + row[98..1000]
     notes_quantity = 0
     notes_sum = 0.0
     notes.each do |note|
