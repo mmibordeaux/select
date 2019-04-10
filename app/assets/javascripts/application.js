d7c16307507e7e7c91a80611c0664901aca16ed1
Arnaud Levy <alevy@lespoupees.paris>
2019-04-10 08:30:46 +0200

    Fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,63 +11,66 @@
 
 jQuery(function ($) {
     'use strict';
-    var $content = $('#formulaire textarea').html().replace(/\n/g, "<br />");
-    $('#formulaire textarea').replaceWith($content);
-    $('#parcoursup').autolink();
-    $('.synthesis textarea').replaceWith($content);
-    $('.synthesis').autolink();
+    if ($('#formulaire').length > 0) {
 
-    $('.marked').mark([
-        'html',
-        'python',
-        'php',
-        'css',
-        'mooc',
-        'tournage',
-        'developpement',
-        'javascript',
-        'openclassroom',
-        'jeu vidéo',
-        'jeux vidéos',
-        'webmaster',
-        'reseaux sociaux',
-        'communication',
-        'sécurité',
-        'community manager',
-        'design graphique',
-        'courts-métrages',
-        'montage',
-        'site web',
-        'photoshop',
-        'illustrator',
-        'audiovisuel',
-        'stage',
-        'cadrage',
-        'prise de son',
-        'mao',
-        'programmation',
-        'infographie',
-        'web design',
-        'photographe',
-        'artistique',
-        'seo',
-        'indesign',
-        'photographie',
-        'web marketing',
-        'anglais',
-        'game designer',
-        'webdesign',
-        'motion',
-        'métier',
-        'groupe',
-        'équipe',
-        'autodidacte',
-        'apprendre',
-        'apprentissage',
-        'portes ouvertes',
-        'jpo'
-    ], {
-        separateWordSearch: false,
-        exclude: ['.labelInputText', '.titreCkeditor']
-    });
+        var $content = $('#formulaire textarea').html().replace(/\n/g, "<br />");
+        $('#formulaire textarea').replaceWith($content);
+        $('#parcoursup').autolink();
+        $('.synthesis textarea').replaceWith($content);
+        $('.synthesis').autolink();
+
+        $('.marked').mark([
+            'html',
+            'python',
+            'php',
+            'css',
+            'mooc',
+            'tournage',
+            'developpement',
+            'javascript',
+            'openclassroom',
+            'jeu vidéo',
+            'jeux vidéos',
+            'webmaster',
+            'reseaux sociaux',
+            'communication',
+            'sécurité',
+            'community manager',
+            'design graphique',
+            'courts-métrages',
+            'montage',
+            'site web',
+            'photoshop',
+            'illustrator',
+            'audiovisuel',
+            'stage',
+            'cadrage',
+            'prise de son',
+            'mao',
+            'programmation',
+            'infographie',
+            'web design',
+            'photographe',
+            'artistique',
+            'seo',
+            'indesign',
+            'photographie',
+            'web marketing',
+            'anglais',
+            'game designer',
+            'webdesign',
+            'motion',
+            'métier',
+            'groupe',
+            'équipe',
+            'autodidacte',
+            'apprendre',
+            'apprentissage',
+            'portes ouvertes',
+            'jpo'
+        ], {
+            separateWordSearch: false,
+            exclude: ['.labelInputText', '.titreCkeditor']
+        });
+    }
 });
