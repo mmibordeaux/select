33710afc6effe9bd0a837891a4e31b627552b087
Arnaud Levy <alevy@lespoupees.paris>
2019-05-03 19:15:00 +0200

    selection

--- app/controllers/candidates/selections_controller.rb
+++ app/controllers/candidates/selections_controller.rb
@@ -1,10 +1,8 @@
 class Candidates::SelectionsController < ApplicationController
 
   def index
-    @candidates =  Candidate.ordered_by_selection
-                            .selected
+    @candidates =  Candidate.selected
     @candidates = @candidates.search params[:search] if params.has_key? :search
-    @candidates = @candidates.page params[:page]
   end
 
   def stats
