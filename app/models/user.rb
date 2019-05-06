8e1f67cd9d3b2d6d0501d7368d01ff0ca00d28e1
Arnaud Levy <alevy@lespoupees.paris>
2019-05-06 18:28:38 +0200

    not registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :candidates_evaluated, class_name: 'Candidate', foreign_key: :evaluated_by
