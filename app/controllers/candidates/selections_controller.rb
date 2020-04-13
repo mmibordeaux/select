bd41b34d469f02b9f8b4fd4ef4d9529d145ee541
Arnaud Levy <contact@arnaudlevy.com>
2020-04-13 10:53:43 +0200

    tech/gen

--- app/controllers/candidates/selections_controller.rb
+++ app/controllers/candidates/selections_controller.rb
@@ -3,6 +3,10 @@ class Candidates::SelectionsController < ApplicationController
   def index
     @candidates =  Candidate.selection_selected.ordered_by_selection
     @candidates = @candidates.search params[:search] if params.has_key? :search
+    if params.has_key? :baccalaureat_id
+      @baccalaureat = Baccalaureat.find params[:baccalaureat_id]
+      @candidates = @candidates.where(baccalaureat_id: @baccalaureat.children_ids)
+    end
   end
 
   def stats
