07083172f07f64e82506c180f8b8184ea32d3b20
Arnaud Levy <contact@arnaudlevy.com>
2020-04-13 11:06:37 +0200

    title

--- app/controllers/candidates/selections_controller.rb
+++ app/controllers/candidates/selections_controller.rb
@@ -1,11 +1,14 @@
 class Candidates::SelectionsController < ApplicationController
 
   def index
+    @title = 'Sélection'
     @candidates =  Candidate.selection_selected.ordered_by_selection
     @candidates = @candidates.search params[:search] if params.has_key? :search
     if params.has_key? :baccalaureat_id
       @baccalaureat = Baccalaureat.find params[:baccalaureat_id]
       @candidates = @candidates.where(baccalaureat_id: @baccalaureat.children_ids)
+      @title += @baccalaureat.id == 113 ? ' bacs technologiques'
+                                        : ' bacs généraux'
     end
   end
 
