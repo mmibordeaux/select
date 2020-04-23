e980e1a87e167f79b8e61be18557b8da2d58a5ce
Arnaud Levy <contact@arnaudlevy.com>
2020-04-23 18:26:20 +0200

    fix

--- app/controllers/evaluations/my_controller.rb
+++ app/controllers/evaluations/my_controller.rb
@@ -3,6 +3,6 @@ class Evaluations::MyController < ApplicationController
     @candidates = current_user.candidates
     @evaluations = current_user.evaluations
     @evaluations_todo = @evaluations.todo.page params[:page_todo]
-    @evaluations_done = @evaluations.done.page params[:page_todo]
+    @evaluations_done = @evaluations.done.page params[:page_done]
   end
 end
