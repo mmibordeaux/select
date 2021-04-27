ac3df83966302ac61cad2b287d3bfa7442ee6904
Arnaud Levy <contact@arnaudlevy.com>
2021-04-27 11:47:13 +0200

    fix

--- app/controllers/candidates/selections_controller.rb
+++ app/controllers/candidates/selections_controller.rb
@@ -4,11 +4,11 @@ class Candidates::SelectionsController < ApplicationController
     @title = 'Sélection'
     @candidates =  Candidate.selection_selected.ordered_by_selection
     @candidates = @candidates.search params[:search] if params.has_key? :search
-    if params.has_key? :baccalaureat_id
-      @baccalaureat = Baccalaureat.find params[:baccalaureat_id]
+    if params.has_key? :baccalaureats
+
+      @baccalaureat = Baccalaureat.send params[:baccalaureats]
       @candidates = @candidates.where(baccalaureat_id: @baccalaureat.children_ids)
-      @title += @baccalaureat.id == 113 ? ' bacs technologiques'
-                                        : ' bacs généraux'
+      @title += " #{params[:baccalaureats]}"
     end
   end
 
