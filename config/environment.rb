44dfbcd2ea272b3ddc339d797d180eef25bc5099
Arnaud Levy <contact@arnaudlevy.com>
2023-05-04 19:14:17 +0200

    sendinblue

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,9 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  api_key: ENV['MAILGUN_API_KEY'],
-  user_name: ENV['MAILGUN_SMTP_LOGIN'],
-  password: ENV['MAILGUN_SMTP_PASSWORD'],
-  domain: ENV['MAILGUN_DOMAIN'],
-  address: ENV['MAILGUN_SMTP_SERVER'],
-  port: ENV['MAILGUN_SMTP_PORT'],
-  authentication: :plain,
-  enable_starttls_auto: true
+  port:           ENV['SENDINBLUE_PORT'],
+  address:        ENV['SENDINBLUE_SMTP_SERVER'],
+  user_name:      ENV['SENDINBLUE_SMTP_LOGIN'],
+  password:       ENV['SENDINBLUE_SMTP_PASSWORD'],
+  authentication: :plain
 }
