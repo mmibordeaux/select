bd591891dddb20819095163dc69156aa8845e3e5
Arnaud Levy <contact@arnaudlevy.com>
2024-05-05 11:19:21 +0200

    download

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,8 +1,8 @@
 class SignaturesController < ApplicationController
   def show
     id = params[:id]
-    url = "https://drive.usercontent.google.com/download?id=#{id}"
-    download = open(url)
+    url = "https://drive.usercontent.google.com/download?id=#{id}&export=download&authuser=0"
+    download = URI.open(url)
     send_file(download, type: 'image/png', disposition: 'inline')
   end
 end
